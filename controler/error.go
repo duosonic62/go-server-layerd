4d7d1b03f73c4a4826111b172137483ce826a1e2
関口 陸 <sekiguchiriku@team-lab.com>
2019-11-01 11:26:29 +0900

    add error handling

--- controler/error.go
+++ controler/error.go
@@ -1,11 +1,46 @@
 package controler
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+)
 
-type apiError struct {
+type ApiError interface {
+	Error() string
+	GetCode() int
+	GetMessage() string
+}
+
+type ApplicationError struct {
 	Message string
+	Code    int
+}
+
+func New(code int, message string) ApplicationError {
+	return ApplicationError{
+		Code:    code,
+		Message: message,
+	}
+}
+
+func (appError ApplicationError) Error() string {
+	return appError.Message
+}
+
+func (appError ApplicationError) GetCode() int {
+	return appError.Code
+}
+
+func (appError ApplicationError) GetMessage() string {
+	errors.New()
+	return appError.Message
 }
 
 func commonError(context *gin.Context, err error) {
-	context.JSON(500, err)
+	switch e := err.(type) {
+	case *ApplicationError:
+		context.JSON(e.Code, err)
+	default:
+		context.JSON(500, New(500, e.Error()))
+	}
 }
